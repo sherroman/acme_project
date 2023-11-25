da1150eef4bf9d77758ddef0994a3de57b51d50f
Роман Щербаков <sherromanrs@yandex.ru>
2023-11-25 22:58:46 +0300

    validation request.GET

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
 
 
 def birthday(request):
-    form = BirthdayForm()
-    return render(
-        request,
-        'birthday/birthday.html',
-        {'form': form}
-    )
+    form = BirthdayForm(request.GET or None)
+    context = {'form': form}
+    if form.is_valid():
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        # Обновляем словарь контекста: добавляем в него новый элемент.
+        context.update({'birthday_countdown': birthday_countdown})
+
+    return render(request, 'birthday/birthday.html', context)
