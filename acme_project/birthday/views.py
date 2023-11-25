6a89b9af4b17906b057eb035e9d3ebf558fb31c9
Роман Щербаков <sherromanrs@yandex.ru>
2023-11-25 21:20:22 +0300

    add form to birthday.html

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from .forms import BirthdayForm
 
 
 def birthday(request):
-    context = {}
+    form = BirthdayForm()
     return render(
         request,
         'birthday/birthday.html',
-        context=context
+        {'form': form}
     )
