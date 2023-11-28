8d1611b9ec9f7a18ef9500c83f47c297f1b40f38
Роман Щербаков <sherromanrs@yandex.ru>
2023-11-28 20:16:26 +0300

    token defense, form post and birthday list

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
+from .models import Birthday
 
 
 def birthday(request):
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST or None)
     context = {'form': form}
     if form.is_valid():
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
             # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
@@ -15,3 +17,11 @@ def birthday(request):
         context.update({'birthday_countdown': birthday_countdown})
 
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
