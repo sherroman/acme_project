ec28806d00d4899f1620a08f7d3aa11729f67d0e
Роман Щербаков <sherromanrs@yandex.ru>
2023-11-30 00:29:15 +0300

    use form to edit objects from DB

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,22 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
 from .forms import BirthdayForm
-from .utils import calculate_birthday_countdown
 from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def birthday(request, pk=None):
+    # Если в запросе указан pk (если получен запрос на редактирование объекта):
+    if pk is not None:
+        # Получаем объект модели или выбрасываем 404 ошибку.
+        instance = get_object_or_404(Birthday, pk=pk)
+    # Если в запросе не указан pk
+    # (если получен запрос к странице создания записи):
+    else:
+        # Связывать форму с объектом не нужно, установим значение None.
+        instance = None
+    # Передаём в форму либо данные из запроса, либо None. 
+    # В случае редактирования прикрепляем объект модели.
+    form = BirthdayForm(request.POST or None, instance=instance)
+    # Остальной код без изменений.
     context = {'form': form}
+    # Сохраняем данные, полученные из формы, и отправляем ответ:
     if form.is_valid():
         form.save()
         birthday_countdown = calculate_birthday_countdown(
-            # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
         )
-        # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
-
-    return render(request, 'birthday/birthday.html', context)
+    return render(request, 'birthday/birthday.html', context) 
 
 
 def birthday_list(request):
