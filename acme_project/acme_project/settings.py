78d4a92ee497a337263c747d159073ffcfaa59c2
Роман Щербаков <sherromanrs@yandex.ru>
2023-11-30 21:00:44 +0300

    form for delete

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e'
+SECRET_KEY = 'django-insecure'
+'-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e'
 
 DEBUG = True
 
@@ -28,7 +29,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'acme_project.urls'
@@ -62,16 +62,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BirthdayForm
 from .models import Birthday
@@ -15,7 +15,7 @@ def birthday(request, pk=None):
     else:
         # Связывать форму с объектом не нужно, установим значение None.
         instance = None
-    # Передаём в форму либо данные из запроса, либо None. 
+    # Передаём в форму либо данные из запроса, либо None.
     # В случае редактирования прикрепляем объект модели.
     form = BirthdayForm(request.POST or None, instance=instance)
     # Остальной код без изменений.
@@ -27,7 +27,7 @@ def birthday(request, pk=None):
             form.cleaned_data['birthday']
         )
         context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context) 
+    return render(request, 'birthday/birthday.html', context)
 
 
 def birthday_list(request):
@@ -36,3 +36,20 @@ def birthday_list(request):
     # Передаём их в контекст шаблона.
     context = {'birthdays': birthdays}
     return render(request, 'birthday/birthday_list.html', context)
+
+
+def delete_birthday(request, pk):
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        # ...и переадресовываем пользователя на страницу со списком записей.
+        return redirect('birthday:list')
+    # Если был получен GET-запрос — отображаем форму.
+    return render(request, 'birthday/birthday.html', context)
