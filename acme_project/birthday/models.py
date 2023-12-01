ce7cc9a427bda26934db62fbbe5237c79143ee9f
Роман Щербаков <sherromanrs@yandex.ru>
2023-12-01 16:38:03 +0300

    validators and cleaned_data

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .validators import real_age
 
 
 class Birthday(models.Model):
@@ -9,4 +10,12 @@ class Birthday(models.Model):
         help_text='Необязательное поле',
         max_length=20
     )
-    birthday = models.DateField('Дата рождения')
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
