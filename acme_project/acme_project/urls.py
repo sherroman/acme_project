4aa44903cc45aa356f8a80720d041d6de39e412a
Роман Щербаков <sherromanrs@yandex.ru>
2023-12-06 23:11:31 +0300

    add cbv to registration

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,7 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+
+
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
+from django.urls import include, path, reverse_lazy
+
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -9,4 +15,13 @@ urlpatterns = [
     path('birthday/', include('birthday.urls')),
     # Подключаем urls.py приложения для работы с пользователями.
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
